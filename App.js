196941497d0d8c82323f9ccad8daedd3bc6efb1b
Adam Falchetta <adamf1631@yahoo.com>
2020-08-30 19:13:09 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/header";
+import Photos from "./components/photos";
+
+function App() {
+	return (
+		<div className='App'>
+			<Header />
+			<Photos />
+		</div>
+	);
+}
+
+export default App;
